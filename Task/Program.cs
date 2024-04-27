a23e05d4cd96b3e020165f17e9a96a048645aee1
Dmitry <rasma1@mail.ru>
2024-04-27 20:42:53 +0300

    ReduceCopyArray method

--- Task/Program.cs
+++ Task/Program.cs
@@ -10,7 +10,32 @@
     return array;
 }
 
+string[] ReduceCopyArray(string[] array, int numMaxStringLetters)
+{
+    int numShortElm = 0;
 
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= numMaxStringLetters)
+        {
+            numShortElm ++;
+        }
+    }
+
+    string[] ResultArray = new string[numShortElm];
+    int resArrayIndex = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= numMaxStringLetters)
+        {
+            ResultArray[resArrayIndex] = array[i];
+            resArrayIndex ++;
+        }
+    }
+
+    return ResultArray;
+}
 
 void PrintArray(string[] array)
 {
@@ -25,3 +50,5 @@ void PrintArray(string[] array)
 
 string[] InitArray = CreateArray(5);
 PrintArray(InitArray);
+string[] ResArray = ReduceCopyArray(InitArray, 3);
+PrintArray(ResArray);
